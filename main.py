638ecb22cf40233fe3d3017971e49805e0be2e8e
LilVos <107175479+LilVos@users.noreply.github.com>
2024-06-23 21:08:41 +0200

    tesxt

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ pygame.init()
 #create game window
 screen = pygame.display.set_mode((800,600))
 
+# game loop
+run = True
+while run:
+    
+    for event in pygame.event.get():
+        #quit
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
