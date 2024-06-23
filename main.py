39f9deeb0908ad95f185ab1a258c9ff3678f4ecc
LilVos <107175479+LilVos@users.noreply.github.com>
2024-06-23 21:10:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pygame
 #initalize pygame
 pygame.init()
 
+# create clock FPS
+clock = pygame.time.Clock()
+
 
 #create game window
 screen = pygame.display.set_mode((800,600))
@@ -10,6 +13,8 @@ screen = pygame.display.set_mode((800,600))
 # game loop
 run = True
 while run:
+    
+    clock.tick(60)
     
     for event in pygame.event.get():
         #quit
