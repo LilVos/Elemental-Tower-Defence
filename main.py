4d529f8976f262ba927871047c68052026abb850
LilVos <107175479+LilVos@users.noreply.github.com>
2024-06-23 21:12:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ pygame.init()
 # create clock FPS
 clock = pygame.time.Clock()
 
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 500
+FPS = 60
 
 #create game window
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
 # game loop
 run = True
 while run:
     
-    clock.tick(60)
+    clock.tick(FPS)
     
     for event in pygame.event.get():
         #quit
