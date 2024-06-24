52126317422d7145a6e4db30d2b94048be21488f
LilVos <107175479+LilVos@users.noreply.github.com>
2024-06-24 23:21:00 +0200

    sefsefsef

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from enemy import *
+from world import *
+
 
 # Initialize pygame
 pygame.init()
@@ -13,12 +15,22 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Elemental Tower Defence")
 
 # Load images
+
+# Map Image
+
+map_image = pygame.image.load(r"C:\\Users\\User\\Desktop\\PYTHON\\Elemantal Tower Defense\\Elemental-Tower-Defence\\ASSESTS\\map\\map 1.png").convert_alpha()
+
+# Enemy Image
 enemy_image = pygame.image.load(r"C:\\Users\\User\\Desktop\\PYTHON\\Elemantal Tower Defense\\Elemental-Tower-Defence\\ASSESTS\\images.png").convert_alpha()
+
+
+# Create World
+world = World(map_image)
 
 # Create group
 enemy_group = pygame.sprite.Group()
 
-waypoints = [(100, 100), (400, 200), (400, 100), (200, 300), (300, 50)]
+waypoints = [(100, 100), (400, 200), (400, 100), (200, 300), (300, 50),(20,50)]
 
 first_enemy = Enemy(waypoints, enemy_image)
 enemy_group.add(first_enemy)
@@ -32,6 +44,10 @@ while run:
     clock.tick(FPS)
 
     screen.fill("grey100")
+    
+    # Draw Level 
+    world.draw(screen)
+
 
     # Draw enemy path
     pygame.draw.lines(screen, "BLACK", False, waypoints)
